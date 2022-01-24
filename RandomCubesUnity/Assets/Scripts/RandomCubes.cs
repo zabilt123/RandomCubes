211478879681d2bcd7788c2612630da09ed8d712
zabilt123 <zat2520@utulsa.edu>
2022-01-24 15:09:56 -0600

    Cube Setup
    -Created Cube prefab
    -Created start of RandomCubes class

---
+++ RandomCubesUnity/Assets/Scripts/RandomCubes.cs
@@ -0,0 +1,27 @@
+/***
+ * Created by: Zabil Tahir
+ * Date Created: 1/24/22
+ * Last Edited By: Zabil Tahir
+ * Last Edited Date: 1/24/22
+ * 
+ * Description: Spawn multiple cube prefabs into the scene.
+ * **/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
